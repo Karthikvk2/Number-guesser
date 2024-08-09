8d311d1f8cab999683e483e3ca44f6900b3db9a5
Karthik V Kottary <95745036+Karthikvk2@users.noreply.github.com>
2024-08-09 21:45:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+
+def check_answer(user_guess, actual_answer, turns):
+    if user_guess > actual_answer:
+        print("Too high!")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too Low")
+        return turns - 1
+    else:
+        print(f"You got it! The answer is {actual_answer}")
+
+
+def set_difficulty():
+    level = input("Choose a Difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm Thinking of a number between 1 and 100.")
+    number = randint(1, 100)
+    print(number)
+
+    turns = set_difficulty()
+
+    guess = 0
+    while guess != number:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        # Let the user guess a number
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, number, turns)
+        if turns == 0:
+            print("You have run out of guesses, you lose.")
+            return
+        elif guess != number:
+            print("Guess Again!")
+
+
+game()
